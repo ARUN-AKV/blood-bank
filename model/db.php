f934ad05b61c2c6cf380962761dd2ea4fde01a58
AKV-PC\ARUN <afm447@gmail.com>
2016-10-11 10:32:49 +0530

    completed connection function

--- model/db.php
+++ model/db.php
@@ -4,7 +4,13 @@
 
 	function db_connect(){
 		$connection = mysqli_connect("localhost","root","","blood_bank");
-		
+		if (!$connection) {
+			die("connection failed:".mysqli_connect_error());
+			exit();
+		}
+		return $connection;
 	}
+
+	function execute_query
 
  ?>
