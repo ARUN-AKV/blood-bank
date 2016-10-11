c70e74abffa8d565bada86d51ed3f96fc835b412
AKV-PC\ARUN <afm447@gmail.com>
2016-10-11 12:00:27 +0530

    registration

--- view/registration.php
+++ view/registration.php
@@ -1,11 +1,11 @@
 <?php  include_once 'header.php';  ?>
 	<div class="container">
 		<h1 class="main-header">Register</h1>
-		<input type="text" name="name" placeholder="Name">
-		<input type="text" name="place" placeholder="Place">
-		<input type="email" name="email" placeholder="Email">
-		<input type="text" name="phone_no" placeholder="Phone_No">
-		<input type="text" name="b_group" placeholder="Blood group">
-		<button type="submit">Register</button>
+		<input class="form-control" type="text" name="name" placeholder="Name"><hr>
+		<input class="form-control" type="text" name="place" placeholder="Place"><hr>
+		<input class="form-control" type="email" name="email" placeholder="Email"><hr>
+		<input class="form-control" type="text" name="phone_no" placeholder="Phone_No"><hr>
+		<input class="form-control" type="text" name="b_group" placeholder="Blood group"><hr>
+		<button class="form-control btn btn-success" type="submit">Register</button>
 	</div>
 <?php  include_once 'footer.php';  ?>
