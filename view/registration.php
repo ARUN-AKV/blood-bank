d7705ce7f1f70dd9643c254383ecb7f06912bad8
AKV-PC\ARUN <afm447@gmail.com>
2016-10-11 11:51:26 +0530

    updated

---
+++ view/registration.php
@@ -0,0 +1,11 @@
+<?php  include_once 'header.php';  ?>
+	<div class="container">
+		<h1 class="main-header">Register</h1>
+		<input type="text" name="name" placeholder="Name">
+		<input type="text" name="place" placeholder="Place">
+		<input type="email" name="email" placeholder="Email">
+		<input type="text" name="phone_no" placeholder="Phone_No">
+		<input type="text" name="b_group" placeholder="Blood group">
+		<button type="submit">Register</button>
+	</div>
+<?php  include_once 'footer.php';  ?>
--- view/resgistration.php
+++
@@ -1,7 +0,0 @@
-<?php  include_once 'header.php';  ?>
-	<div class="container">
-		<h1 class="main-header">Register</h1>
-		<input type="text" name="name" placeholder="Name">
-		<input type="" name="">
-	</div>
-<?php  include_once 'footer.php';  ?>
