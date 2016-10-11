f2cc5fa2649aee74c8ddb7f96324bde460dd3e88
AKV-PC\ARUN <afm447@gmail.com>
2016-10-11 14:10:57 +0530

    reg update

--- view/registration.php
+++ view/registration.php
@@ -1,12 +1,12 @@
 <?php  include_once 'header.php';  ?>
 	<form action="../controller/reg.php" method="post">
 		<div class="container">
-			<h1 class="jumbotron text-center">Register</h1>
-			<input class="form-control" type="text" name="name" placeholder="Name" required><hr>
-			<input class="form-control" type="text" name="place" placeholder="Place" required><hr>
-			<input class="form-control" type="email" name="email" placeholder="Email" required><hr>
-			<input class="form-control" type="text" name="phone_no" placeholder="Phone_No" required><hr>
-			<input class="form-control" type="text" name="b_group" placeholder="Blood group" required><hr>
+			<h1 class="main-header text-center">Register</h1>
+			<input class="form-control" type="text" name="name" placeholder="Name" required><br>
+			<input class="form-control" type="text" name="place" placeholder="Place" required><br>
+			<input class="form-control" type="email" name="email" placeholder="Email" required><br>
+			<input class="form-control" type="text" name="phone_no" placeholder="Phone_No" required><br>
+			<input class="form-control" type="text" name="b_group" placeholder="Blood group" required><br>
 			<button class="form-control btn btn-success" type="submit">Register</button>
 		</div>
 	</form>
