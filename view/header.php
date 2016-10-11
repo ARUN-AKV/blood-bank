0154fd61776667b366777a458bbe64ac9f6bf151
AKV-PC\ARUN <afm447@gmail.com>
2016-10-11 11:34:17 +0530

    header footer added

---
+++ view/header.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title>home</title>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/jquery.ui.css">
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="../js/jquery-ui.js"></script>
+	<script type="text/javascript"src="../js/jquery.min.js"></script>
+</head>
+<body>
