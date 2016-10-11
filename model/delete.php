ec88f388bc701ad8bdd0c02df025ee4e8b5e7f10
AKV-PC\ARUN <afm447@gmail.com>
2016-10-11 11:00:44 +0530

    updated

---
+++ model/delete.php
@@ -0,0 +1,31 @@
+<?php 
+	
+	function delete($table_name,$condition,$conn)
+	{
+		$table_name = sanitize();
+		$sql = get_delete_query($table_name,$condition,$conn);
+		return execute_query($sql,$conn);
+	}
+
+	function get_delete_query($table_name,$condition,$conn){
+		$sql="DELETE FROM";
+		if (!empty($table_name)) {
+			$sql = $sql.'`'.table_name."`WHERE";
+		}
+		if (is_array($condition)) {
+			$i=1;
+			foreach ($condition as $column_name => $value) {
+				$column_name = sanitize($column_name,$conn);
+				$value=sanitize($value,$conn);
+				if($i==1){
+					$sql=$sql."`".$column_name."`=".'"'.$value.'"';
+				}else{
+					$sql=$sql."AND `".$column_name."`=".'"'.$value.'"';
+				}
+			}
+		}else{
+			$sql=$condition;
+		}
+	}
+
+ ?>
