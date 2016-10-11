580771fa996cea53ac4fdf0712bcdad95e469550
AKV-PC\ARUN <afm447@gmail.com>
2016-10-11 21:13:42 +0530

    updated

--- controller/search.php
+++ controller/search.php
@@ -1,3 +1,4 @@
+<?php include_once '../view/header.php'; ?>
 <?php 
 	include_once '../model/curd_operations.php';
 	include_once '../model/db.php';
@@ -5,10 +6,25 @@
 	$raw_values = $_POST;
 	$sql= "SELECT * FROM donor_info WHERE b_group like '".$raw_values['bg']."'";
 	$result = mysqli_query($conn,$sql);
-	create_session($sql);
-	if (!$result) {
-		echo 'no data found';
-	}else{
-		header('Location:../view/report.php');
-	}
- ?>
+	echo '<div class="page-header text-center "><h1>Report</h1></div> 
+  <table class="table table-hover table-responsive">
+  <thead>
+  	<th>S.no</th>
+    <th class="col-md-2">Name</th>
+    <th>Place</th>
+    <th>Phone no</th>
+    <th>Email Id</th>
+  </thead>';
+  $i=1;
+  while($row = mysqli_fetch_assoc($result))
+  {
+  echo "<tr>";
+  echo "<td>".$i."</td>";
+  echo "<td>".$row['name']."</td>";
+  echo "<td>".$row['place']."</td>";
+  echo "<td>".$row['phone_no']."</td>";
+  echo "<td>".$row['email']."</td>";
+  echo "</tr>";
+  ++$i;
+}
+?>
