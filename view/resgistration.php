5ec2ee5c58a9ed4c7f70dc8dcb639955ec7c8a73
AKV-PC\ARUN <afm447@gmail.com>
2016-10-11 11:47:08 +0530

    addin register page

---
+++ view/resgistration.php
@@ -0,0 +1,7 @@
+<?php  include_once 'header.php';  ?>
+	<div class="container">
+		<h1 class="main-header">Register</h1>
+		<input type="text" name="name" placeholder="Name">
+		<input type="" name="">
+	</div>
+<?php  include_once 'footer.php';  ?>
