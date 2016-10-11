14fef37ed8f9ae99236e85b35be71c9711638228
AKV-PC\ARUN <afm447@gmail.com>
2016-10-11 10:36:03 +0530

    added execute query function

--- model/db.php
+++ model/db.php
@@ -11,6 +11,15 @@
 		return $connection;
 	}
 
-	function execute_query
+	function execute_query($query,$link){
+		if(!empty($link)){
+			return mysqli_query($link,$query);
+		}
+		else{
+			return mysqli_query(db_connect(),$query);
+		}
+	}
 
+	
+	
  ?>
