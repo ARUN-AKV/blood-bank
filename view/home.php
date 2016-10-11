ae075666a4427d2d17189be6b6be008ba12753a2
AKV-PC\ARUN <afm447@gmail.com>
2016-10-11 11:43:10 +0530

    updated home page

---
+++ view/home.php
@@ -0,0 +1,24 @@
+<?php  include_once 'header.php';  ?>
+	
+	<h1 class="main-header" style="text-align: center;">BLOOD BANK HOME PAGE</h1>
+	<section class="container">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</section>
+	<hr>
+	<div class="container">
+		<a class="btn btn-info col-md-6" href="registration.php">Registration</a>
+		<a class="btn btn-danger col-md-6" href="search.php">Search blood</a>
+	</div>
+
+<?php  include_once 'footer.php';  ?>
