9028b3660fb8b7cefdf544d2a95d66e7dd6d587d
AKV-PC\ARUN <afm447@gmail.com>
2016-10-12 12:25:37 +0530

    added nav bar in header

--- view/header.php
+++ view/header.php
@@ -1,10 +1,21 @@
 <html>
+
 <head>
-	<title>home</title>
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="../css/jquery.ui.css">
-	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="../js/jquery-ui.js"></script>
-	<script type="text/javascript"src="../js/jquery.min.js"></script>
+    <title>home</title>
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/jquery.ui.css">
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+    <link rel="stylesheet" type="text/css" href="../css/fonts.css">
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../js/jquery-ui.js"></script>
+    <script type="text/javascript" src="../js/jquery.min.js"></script>
 </head>
+
 <body>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="home.php"><img src="../img/logo.png" alt="BLOOD BANK"></a>
+            </div>
+        </div>
+    </nav>
