a1b8a438b9728a5e66aa5514d4c3bedb76de8512
AKV-PC\ARUN <afm447@gmail.com>
2016-10-12 12:27:19 +0530

    update input boxes

--- view/registration.php
+++ view/registration.php
@@ -3,10 +3,11 @@
 		<div class="container">
 			<h1 class="main-header text-center">Register</h1>
 			<input class="form-control" type="text" name="name" placeholder="Name" required><br>
-			<input class="form-control" type="text" name="place" placeholder="Place" required><br>
+			<input class="form-control" type="text" name="age" placeholder="Age(years)" required><br>
+			<input class="form-control" type="text" name="place" placeholder="Location" required><br>
 			<input class="form-control" type="email" name="email" placeholder="Email" required><br>
 			<input class="form-control" type="text" name="phone_no" placeholder="Phone_No" required><br>
-			<input class="form-control" type="text" name="b_group" placeholder="Blood group" required><br>
+			<input class="form-control" type="text" name="b_group" placeholder="Blood group(Capital letter)" required><br>
 			<button class="form-control btn btn-success" type="submit">Register</button>
 		</div>
 	</form>
--- view/search.php
+++ view/search.php
@@ -2,7 +2,8 @@
 	<form action="../controller/search.php" method="post">
 		<h1 class="main-header text-center">SEARCH FOR BLOOD</h1>
 		<div class="container">
-			<input class="form-control" type="text" name="bg" placeholder="Enter Blood Group"><br>
+			<input class="form-control" type="text" name="bg" placeholder="Enter Blood Group" required><br>
+			<input class="form-control" type="text" name="place" placeholder="Location" required><br>
 			<button class="form-control btn btn-success" type="submit" id="search">Search</button>
 		</div>
 	</form>
