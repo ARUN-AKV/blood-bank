8f092b027ca017ff5620f5a116478e9ba0637923
AKV-PC\ARUN <afm447@gmail.com>
2016-10-13 15:07:06 +0530

    update

--- view/search.php
+++ view/search.php
@@ -1,5 +1,5 @@
 <?php  include_once 'header.php';  ?>
-	<form action="../controller/search.php" method="post">
+	<form action="result.php" method="post">
 		<h1 class="main-header text-center">SEARCH FOR BLOOD</h1>
 		<div class="container">
 			<input class="form-control" type="text" name="bg" placeholder="Enter Blood Group" required><br>
